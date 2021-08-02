437384aed05f0d8424e51b6b0a7733188fc04f00
Avneesh Agarwal <mamtajalan22@gmail.com>
2021-08-02 18:10:25 +0530

    created the tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,69 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Image from "next/image";
+import Links from "../components/Links";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div
+      style={{
+        background:
+          "url(https://images.unsplash.com/photo-1517707711963-adf9078bdf01?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80)",
+      }}
+      className="flex flex-col !bg-cover items-center justify-center min-h-screen py-2"
+    >
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+      <div className="flex flex-col items-center justify-center mt-5">
+        <Image
+          width={200}
+          height={200}
+          className="mx-auto rounded-full"
+          src="https://res.cloudinary.com/dssvrf9oz/image/upload/v1625825953/Avneesh_Avatar_gukdsk.png"
+          alt="Avneesh Agarwal"
+        />
+        <h2 className="my-3 text-3xl font-bold text-center text-indigo-900 md:text-4xl">
+          Avneesh Agarwal
+        </h2>
+      </div>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <Links
+        image="https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg"
+        link="https://www.instagram.com/avneesh.codes/"
+        text="Follow me on Instagram"
+      />
+      <Links
+        image="https://cdn.hashnode.com/res/hashnode/image/upload/v1611902473383/CDyAuTy75.png?auto=compress"
+        link="https://avneesh0612.hashnode.dev/"
+        text="Checkout my blogs on Hashnode"
+      />
+      <Links
+        image="https://cdn.iconscout.com/icon/free/png-512/discord-2474808-2056094.png"
+        link="https://discord.gg/e3sDQjSnDK"
+        text="Join my Discord server"
+      />
+      <Links
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png"
+        link="https://github.com/avneesh0612"
+        text="Look at my code on Github"
+      />
+      <Links
+        image="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-logo-vector-png-clipart-1.png"
+        link="https://twitter.com/AvneeshAgarwa12"
+        text="Follow me on Twitter"
+      />
+      <Links
+        image="https://www.edigitalagency.com.au/wp-content/uploads/Linkedin-logo-icon-png.png"
+        link="https://www.linkedin.com/in/avneesh-agarwal-78312b20a/"
+        text="Connect with me on LinkedIn"
+      />
+      <Links
+        image="https://icons-for-free.com/iconfiles/png/512/suitcase+work+icon-1320165848716624003.png"
+        link="https://avneeshresume.netlify.app/"
+        text="Checkout my resume"
+      />
     </div>
-  )
+  );
 }
